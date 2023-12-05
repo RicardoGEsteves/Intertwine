c7e1fe7a31d9684f4700e8e28ba04c2f2335836e
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-05 23:34:29 +0000

    feat: :sparkles: Create profile and customize server
    - Create logic for users to cerate a server, Check if profile exist if not create one.
    - Create InitialModal component.
    - Add Zod, react-hook-form and shadcn-ui components as needed.
    - Create primary variant in the Button component.
    
    Create profile and server customization features

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "Please enter a server name.",
+    })
+    .max(20),
+  imageUrl: z.string().min(1, {
+    message: "Please enter a server image URL.",
+  }),
+});
+
+const InitialModal = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Server customization
+          </DialogTitle>
+          <DialogDescription className="text-center text-gray-500">
+            Customize your server channel effortlessly with our versatile
+            features. Tailor permissions, adjust settings, and personalize the
+            channel to suit your community&apos;s unique needs. From setting
+            specific access controls to defining channel topics, make your space
+            truly your own. Enhance engagement and collaboration by molding the
+            channel environment to match your vision seamlessly.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                {/* TODO: image upload */}
+              </div>
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-gray-500 dark:text-secondary/70">
+                      Server name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter a server name"
+                        className="bg-gray-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        disabled={isLoading}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default InitialModal;
