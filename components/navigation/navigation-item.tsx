0b01e408246c55064f71ceb26db4c4b08176004c
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-06 22:55:18 +0000

    fix: :art: Modify navigation item structure
    - Add a div and give the parent the group property.
    - Improve user experience.

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -23,29 +23,31 @@ const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
 
   return (
     <ActionTooltip side="right" align="center" label={name}>
-      <Button
-        variant="outline"
-        size="icon"
-        className="group relative flex items-center"
-        onClick={handleClick}
-      >
-        <div
-          className={cn(
-            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-            params?.serverId !== id && "group-hover:h-[20px]",
-            params?.serverId === id ? "h-[36px]" : "h-[8px]"
-          )}
-        />
-        <div
-          className={cn(
-            "relative group flex my-2  h-full w-full rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-            params?.serverId === id &&
-              "bg-primary/10 text-primary rounded-[16px]"
-          )}
+      <div className="group relative flex items-center">
+        <Button
+          variant="outline"
+          size="icon"
+          className=" rounded-full ml-4 overflow-hidden"
+          onClick={handleClick}
         >
-          <Image fill src={imageUrl} alt="Server Image" />
-        </div>
-      </Button>
+          <div
+            className={cn(
+              "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+              params?.serverId !== id && "group-hover:h-[20px]",
+              params?.serverId === id ? "h-[36px]" : "h-[8px]"
+            )}
+          />
+          <div
+            className={cn(
+              "relative group flex my-2  h-full w-full rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+              params?.serverId === id &&
+                "bg-primary/10 text-primary rounded-[16px]"
+            )}
+          >
+            <Image fill src={imageUrl} alt="Server Image" />
+          </div>
+        </Button>
+      </div>
     </ActionTooltip>
   );
 };
