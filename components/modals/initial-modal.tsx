3f5e4374759861d41d7ab093a066839d4cb8db06
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-06 03:52:31 +0000

    feat: :sparkles: Implement create server feature
    - Create server implemented. - Add axios and uuid.
    - Create POST method to our api and write it to supabase postgres db
    
    Create server implemented

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 import {
   Dialog,
@@ -40,6 +42,8 @@ const formSchema = z.object({
 const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
 
+  const router = useRouter();
+
   useEffect(() => {
     setIsMounted(true);
   }, []);
@@ -55,7 +59,15 @@ const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!isMounted) {
