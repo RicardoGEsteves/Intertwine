243caed1dc8dff398bb71c14c6cf0c2674ea2dba
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-10 19:24:39 +0000

    feat: :sparkles: Implement feature chat.
    - Create chat in channels functionality.
    - Implement functionality to get users messages.
    - Create current profile for socket.io using pages architecture and approach.
    - Create Api route for channel messages.
    
    Channel messages feature created.

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,7 +4,7 @@ import { ChannelType } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
 import ChatHeader from "@/components/chat/chat-header";
-// import { ChatInput } from "@/components/chat/chat-input";
+import ChatInput from "@/components/chat/chat-input";
 // import { ChatMessages } from "@/components/chat/chat-messages";
 // import { MediaRoom } from "@/components/media-room";
 import { db } from "@/lib/db";
@@ -47,9 +47,9 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         serverId={channel.serverId}
         type="channel"
       />
-      {/* {channel.type === ChannelType.TEXT && (
+      {channel.type === ChannelType.TEXT && (
         <>
-          <ChatMessages
+          {/* <ChatMessages
             member={member}
             name={channel.name}
             chatId={channel.id}
@@ -62,7 +62,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             }}
             paramKey="channelId"
             paramValue={channel.id}
-          />
+          />*/}
           <ChatInput
             name={channel.name}
             type="channel"
@@ -73,7 +73,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             }}
           />
         </>
-      )} */}
+      )}
       {/* {channel.type === ChannelType.AUDIO && (
         <MediaRoom chatId={channel.id} video={false} audio={true} />
       )} */}
---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import qs from "query-string";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "../ui/button";
+// import { EmojiPicker } from "@/components/emoji-picker";
+
+interface ChatInputProps {
+  apiUrl: string;
+  query: Record<string, any>;
+  name: string;
+  type: "conversation" | "channel";
+}
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+
+const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: apiUrl,
+        query,
+      });
+
+      await axios.post(url, values);
+
+      form.reset();
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative p-4 pb-6">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
+                    className="absolute top-7 left-8 h-[24px] w-[24px] transition rounded-full p-1"
+                    // bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300   flex items-center justify-center
+                  >
+                    <Plus className="text-primary" />
+                  </Button>
+                  <Input
+                    disabled={isLoading}
+                    className="px-14 py-6 "
+                    // bg-zinc-200/90 dark:bg-zinc-700/75 text-zinc-600 dark:text-zinc-200 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0
+                    placeholder={`Message ${
+                      type === "conversation" ? name : "#" + name
+                    }`}
+                    {...field}
+                  />
+                  <div className="absolute top-7 right-8">
+                    {/* <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
+                    /> */}
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default ChatInput;
