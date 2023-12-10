431c1f14527d8032f2422f890aca416773dc9336
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-10 03:38:35 +0000

    feat: :sparkles: Edit existing channel.
    - Implement edit existing channel.
    - Create feature functionality.
    - Create edit-channel-modal.
    - Add edit-channel-modal to model-provider.
    - Add api PATCH method.
    
    Users can now edit their channels name and/or type.

---
+++ components/modals/edit-channel-modal.tsx
@@ -0,0 +1,180 @@
+"use client";
+
+import axios from "axios";
+import qs from "query-string";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { ChannelType } from "@prisma/client";
+import { useEffect } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useModal } from "@/hooks/use-modal-store";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Channel name must be at least 3 characters long." })
+    .max(20)
+    .refine((name) => name.toLowerCase() !== "general", {
+      message: "You cannot use the name 'general', it is reserved.",
+    }),
+  type: z.nativeEnum(ChannelType),
+});
+
+const EditChannelModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "editChannel";
+  const { channel, server } = data;
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      type: channel?.type || ChannelType.TEXT,
+    },
+  });
+
+  useEffect(() => {
+    if (channel) {
+      form.setValue("name", channel.name);
+      form.setValue("type", channel.type);
+    }
+  }, [form, channel]);
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+      await axios.patch(url, values);
+
+      form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleModalClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleModalClose}>
+      <DialogContent className="bg-secondary text-primary p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Edit Channel
+          </DialogTitle>
+          <DialogDescription className="text-center text-gray-500 dark:text-gray-400">
+            Edit <span className=" font-bold">{channel?.name}</span>, you can
+            change the channel name and type here. You cannot use the name
+            &apos;general&apos;, it is reserved.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-gray-500 dark:text-secondary-foreground/70">
+                      Channel Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter channel name, e.g. trips"
+                        className="bg-gray-300/50 dark:bg-secondary-foreground/50 border-0 focus-visible:ring-0 text-primary focus-visible:ring-offset-0 dark:placeholder:text-secondary-foreground/50"
+                        disabled={isLoading}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-gray-500 dark:text-secondary-foreground/70">
+                      Channel Type
+                    </FormLabel>
+
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="bg-gray-300/50 dark:bg-secondary-foreground/50 border-0 focus:ring-0 text-primary ring-offset-0 focus:ring-offset-0 capitalize outline-none">
+                          <SelectValue placeholder="Select a channel type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {Object.values(ChannelType).map((type) => (
+                          <SelectItem
+                            key={type}
+                            value={type}
+                            className="capitalize"
+                          >
+                            {type.toLowerCase()}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-primary-foreground/30 px-6 py-4">
+              <Button disabled={isLoading}>Save</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default EditChannelModal;
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -10,6 +10,7 @@ import CreateChannelModal from "../modals/create-channel-modal";
 import LeaveServerModal from "../modals/leave-server-modal";
 import DeleteServerModal from "../modals/delete-server-modal";
 import DeleteChannelModal from "../modals/delete-channel-modal";
+import EditChannelModal from "../modals/edit-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -32,6 +33,7 @@ export const ModalProvider = () => {
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
